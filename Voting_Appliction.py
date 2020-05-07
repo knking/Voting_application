69b0fd7d43ebb8af3fe8c9e1edd9b755db1218d6
knking <knr020498@gmail.com>
2020-05-07 20:04:20 +0530

    first

---
+++ Voting_Appliction.py
@@ -0,0 +1,24 @@
+import random
+no_of_can=int(input("Enter Number of candidates : "))
+candidate=input(f'Enter {no_of_can} candidates Name : \n').split()
+print("Candidates are :-> ",candidate)
+print("Voting is Going On ...... Please wait for the Results...")
+print("Voting is Finishid.. And wait is Over")
+print("Here are the results")
+
+vots=[]
+
+for k in range(0,no_of_can):
+    c=random.randrange(1,100,3)
+    vots.append(c)
+
+for n in range(no_of_can):
+    print(candidate[n], ":",vots[n])
+
+print("Hence The Winner is ....")
+maximum=max(vots)
+count=0
+for r in range(no_of_can):
+    if vots[r]==maximum:
+        count =r
+print(candidate[count],":",maximum)
